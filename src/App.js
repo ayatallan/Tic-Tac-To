3d5491a13658d889d5f28f615af56f311d677dc6
Ayat AbuAllan <82124628+ayatallan@users.noreply.github.com>
2022-10-22 16:12:33 +0300

    commit all changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from './component/board/board';
+// import Board from './component/board/board';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>A win is a win , lets play &#x1F60E;</h1>
+      <Board/>
+    </div>
+  );
+}
+
+export default App;
